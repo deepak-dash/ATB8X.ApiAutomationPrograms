7e09f9ffd56015c6fc758ebd8da35deb96113d0f
deepak-dash <deepak.dash930@gmail.com>
2024-09-27 00:48:47 +0530

    Api Automation RestAssured

---
+++ src/main/java/org/apiautomationdeepak/App.java
@@ -0,0 +1,13 @@
+package org.apiautomationdeepak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/apiautomationdeepak/Test001.java
@@ -0,0 +1,10 @@
+package org.apiautomationdeepak;
+
+import io.restassured.RestAssured;
+
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+    }
+}
